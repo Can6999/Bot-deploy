02f37303f722a022451e23658be191e0b6110615
Can6999 <prohasan6999@gmail.com>
2025-03-01 01:33:36 +0700

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -171,7 +171,7 @@ CONTRACT_INFO_FILE = "contract_info.txt"
 def install_foundry_dependencies():
     if not os.path.exists("lib"):
         print("[+] Installing Foundry Dependencies...")
-        subprocess.run(["forge", "install"], check=True)
+        subprocess.run(["forge install --no-commit"], check=True)
         print("[✓] Foundry Dependencies Installed")
     else:
         print("[✓] Foundry Dependencies Already Installed")
