4f5f35a600afccde607220c43048bbb84ec56b6c
Can6999 <prohasan6999@gmail.com>
2025-03-01 14:33:11 +0700

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,8 +2,9 @@ import os
 import subprocess
 from web3 import Web3
 from eth_account import Account
-from can_utils.banner import banner
-banner()
+from can_utils import can
+
+can() 
 
 
 # ----------------------------------------------------------------------
